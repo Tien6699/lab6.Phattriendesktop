0746594a3f2874c39bc6d0933619839189cb816d
tiên <nguyendangnhattien4@gmail.com>
2025-11-10 13:59:55 +0700

    Add files via upload

---
+++ lab6/BusinessLogic/AccountBL.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class AccountBL
+    {
+        AccountDA accountDA = new AccountDA();
+
+        public List<Account> GetAll()
+        {
+            return accountDA.GetAll();
+        }
+
+        public Account GetByID(string accountName)
+        {
+            return accountDA.GetByID(accountName);
+        }
+
+        public List<Account> Find(string key)
+        {
+            List<Account> list = GetAll();
+            List<Account> result = new List<Account>();
+
+            foreach (var item in list)
+            {
+                if (item.AccountName.Contains(key) ||
+                    item.FullName.Contains(key) ||
+                    item.Email.Contains(key))
+                    result.Add(item);
+            }
+            return result;
+        }
+
+        public int Insert(Account account)
+        {
+            return accountDA.Insert(account);
+        }
+
+        public int Update(Account account)
+        {
+            return accountDA.Update(account);
+        }
+
+        public int Delete(string accountName)
+        {
+            return accountDA.Delete(accountName);
+        }
+
+        public bool Login(string username, string password)
+        {
+            var account = GetByID(username);
+            return account != null && account.Password == password;
+        }
+    }
+}
---
+++ lab6/BusinessLogic/RoleBL.cs
@@ -0,0 +1,61 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class RoleBL
+    {
+        RoleDA roleDA = new RoleDA();
+
+        public List<Role> GetAllRoles()
+        {
+            return roleDA.GetAllRoles();
+        }
+
+        public List<RoleAccount> GetUserRoles(string accountName)
+        {
+            return roleDA.GetUserRoles(accountName);
+        }
+
+        public int UpdateUserRoles(string accountName, List<RoleAccount> roles)
+        {
+            return roleDA.UpdateUserRoles(accountName, roles);
+        }
+
+        public bool HasPermission(string accountName, string roleName)
+        {
+            var userRoles = GetUserRoles(accountName);
+            var allRoles = GetAllRoles();
+
+            foreach (var userRole in userRoles)
+            {
+                var role = allRoles.Find(r => r.ID == userRole.RoleID);
+                if (role != null && role.RoleName == roleName && userRole.Actived)
+                    return true;
+            }
+            return false;
+        }
+
+        public List<string> GetUserRoleNames(string accountName)
+        {
+            List<string> roleNames = new List<string>();
+            var userRoles = GetUserRoles(accountName);
+            var allRoles = GetAllRoles();
+
+            foreach (var userRole in userRoles)
+            {
+                if (userRole.Actived)
+                {
+                    var role = allRoles.Find(r => r.ID == userRole.RoleID);
+                    if (role != null)
+                        roleNames.Add(role.RoleName);
+                }
+            }
+            return roleNames;
+        }
+    }
+}
